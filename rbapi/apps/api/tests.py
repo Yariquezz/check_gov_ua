a18add115a146cffa25762e2feda75ee5af4f2cf
yarique <yariuezz@gmail.com>
2020-06-06 02:03:20 +0300

    update requirements'

--- rbapi/apps/api/tests.py
+++ rbapi/apps/api/tests.py
@@ -1,3 +1,80 @@
 from django.test import TestCase
 
-# Create your tests here.
+import requests
+import json
+import socket
+import hashlib
+import hmac
+import base64
+from datetime import datetime
+
+
+def signature(check, time, word):
+    message = bytes("{}{}".format(check, time), 'utf-8')
+    secret = bytes(word, 'utf-8')
+    sign = base64.b64encode(hmac.new(secret, message, digestmod=hashlib.sha256).digest()).decode('utf-8')
+
+    return sign
+
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+
+def run(check):
+
+    ip = get_ip()
+    time = str(int(datetime.now().timestamp()))
+    password = signature(check=check, time=time, word='qwerty-12345')
+
+    params = {}
+    url = 'http://localhost:8000/api/check'
+    response = requests.get(url=url, json=params,
+                            headers=
+                            {
+                                'Content-Type': 'application/json',
+                                'x-time': time,
+                                'x-check-id': check,
+                                'x-real-ip': ip,
+                                'x-hmac': password
+                            }).text
+    return response
+
+
+ck = ['634338963', '978432432', '445104511', '3456234234', '2424234234', '234234234234', '234234234234']
+
+for j in ck:
+    check_num = j
+    resp = ""
+    my_response = json.loads(run(check_num))
+
+    try:
+        doc = {
+            "sender": "платник",
+            "recipient": "отримувач",
+            "amount": "сумма",
+            "date": "дата",
+            "description": "призначення",
+            "currencyCode": "валюта",
+            "comissionRate": "комісія",
+            "link_code": "скачати квитанцію тут"
+        }
+        for i in my_response['payments'][0]:
+            resp += ("{}{} {}\n".format(doc[i], ":", my_response['payments'][0][i]))
+    except Exception as e:
+        print(e)
+    else:
+        print(resp)
+
+# my_response = (run('634338963'))
+# print(my_response)
+
--- rbapi/rbapi/urls.py
+++ rbapi/rbapi/urls.py
@@ -17,10 +17,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('apps.api.urls')),
 ]
