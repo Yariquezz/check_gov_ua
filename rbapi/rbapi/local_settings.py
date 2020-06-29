00a831299a8054167b71362a1ca333b69ebbbf23
yarique <yariuezz@gmail.com>
2020-06-29 23:01:00 +0300

    add whitenoice

--- rbapi/rbapi/local_settings.py
+++ rbapi/rbapi/local_settings.py
@@ -15,10 +15,17 @@ DATABASES = {
     }
 }
 
-#STATIC_ROOT = os.path.join(BASE_DIR, "/static")
-
 API_KEY = 'qwerty-12345'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'ec2-18-196-1-11.eu-central-1.compute.amazonaws.com', '18.196.1.11']
 
 SECRET_KEY = 'lob2t$8)n*-lh#40k7$d5v(y4vlm1%v%q_%ci261^g=q)(!--c'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "/static")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
