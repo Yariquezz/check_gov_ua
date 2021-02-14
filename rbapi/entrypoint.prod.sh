17174aa4bd160ac10ded351d9ac8c1e69a31ca8d
yarique <yariuezz@gmail.com>
2021-02-14 18:02:42 +0200

    create superuser in docker

--- rbapi/entrypoint.prod.sh
+++ rbapi/entrypoint.prod.sh
@@ -15,6 +15,8 @@ python manage.py flush --no-input
 echo "Makemigrations..."
 python manage.py makemigrations
 echo "Migrate..."
+python manage.py createsuperuser --username=$SUPERUSER_ADMIN --password=$SUPERUSER_PASSWORD --email=$SUPERUSER_EMAIL
+echo "Create superuser..."
 python manage.py migrate
 echo "Collectstatic..."
 python manage.py collectstatic --no-input
