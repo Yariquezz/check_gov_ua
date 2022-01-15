87f2e1f68021b843ec97d93d0ad08e290aa057b7
Ярослав Жуковский <yarique@MacBook-Pro.local>
2022-01-15 11:59:40 +0200

    fix some settings dependencies

--- rbapi/rbapi/settings.py
+++ rbapi/rbapi/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import os
+import socket
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-SECRET_KEY = os.environ.get("SECRET_KEY", 'lob2t$8)n*-lh#40k7$d5v(y4vlm1%v%q_%ci261^g=q)(!--c')
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", 'lob2t$8)n*-lh#40k7$d5v(y4vlm1%v%q_%ci261^g=q)(!--c')
 API_KEY = os.environ.get("API_KEY", '12345')
 DEBUG = int(os.environ.get("DEBUG", default=0))
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(" ")
-PERMITTED_HOSTS = os.environ.get("PERMITTED_HOSTS", ['192.168.0.105', '192.168.0.100', '192.168.0.102'])
-HOSTNAME = os.environ.get('HOSTNAME', default='localhost:8000')
+try:
+    HOSTNAME = socket.gethostname()
+except:
+    HOSTNAME = 'localhost'
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Application definition
@@ -65,7 +69,7 @@ DATABASES = {
         "NAME": os.environ.get("SQL_DATABASE", "rbapi"),
         "USER": os.environ.get("SQL_USER", "django"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "django"),
-        "HOST": os.environ.get("SQL_HOST", "192.168.0.108"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
@@ -187,5 +191,3 @@ LOGGING = {
 
     }
 }
-
-X_FRAME_OPTIONS = 'SAMEORIGIN'
